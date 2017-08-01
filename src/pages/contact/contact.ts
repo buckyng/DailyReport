940f94ce7a23f71c60a93b8009282bb85278c804
Bucky Nguyen <bucky.nguyen@caseware.com>
2017-08-01 17:42:18 -0400

    add contact page

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {AlertController} from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +8,70 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  notes: any = [];
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
+
+   addNote(){
+ 
+        let prompt = this.alertCtrl.create({
+            title: 'Add Name',
+            inputs: [{
+                name: 'title'
+            }],
+            buttons: [
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: 'Add',
+                    handler: data => {
+                        this.notes.push(data);
+                    }
+                }
+            ]
+        });
+ 
+        prompt.present();
+    }
+ 
+    editNote(note){
+ 
+        let prompt = this.alertCtrl.create({
+            title: 'Edit Name',
+            inputs: [{
+                name: 'title'
+            }],
+            buttons: [
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        let index = this.notes.indexOf(note);
+ 
+                        if(index > -1){
+                          this.notes[index] = data;
+                        }
+                    }
+                }
+            ]
+        });
+ 
+        prompt.present();       
+ 
+    }
+ 
+    deleteNote(note){
+ 
+        let index = this.notes.indexOf(note);
+ 
+        if(index > -1){
+            this.notes.splice(index, 1);
+        }
+    }
 
 }
